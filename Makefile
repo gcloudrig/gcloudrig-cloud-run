a1e7ad7299d5fba4ea43fe9077af2ad688618d18
putty182 <putty182@gmail.com>
2021-02-22 11:46:58 +0000

    first pass getting gitpod up and running

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	docker build --build-arg project_id=massive-seer-267723 --tag gcloudrigapi .
+	docker build --build-arg project_id="${GOOGLE_CLOUD_PROJECT}" --tag gcloudrigapi .
 
 run_bash:
 	docker run -p 5000:5000 --env PORT=5000 --env JWT_SECRET=testing1234 --env API_USERNAME=test --env API_PASSWORD=password123 -it gcloudrigapi bash
--- service_account.sh
+++ service_account.sh
@@ -1,10 +1,38 @@
+#!/usr/bin/env bash
+
+# ensure we're logged in to gcloud
+LOGGED_IN_USER=$(gcloud auth list --quiet --filter 'status=ACTIVE' --format 'value(account)' 2>/dev/null)
+if [ -z "$LOGGED_IN_USER" ]; then
+    echo "ERROR: Not logged in to gcloud; please run 'gcloud auth login'"
+    exit 1
+fi
+
+# ensure we have a project set (either through env var or via 'gcloudrig config set project')
+if [ -z "$GOOGLE_CLOUD_PROJECT" ]; then
+    gcloud_config_project=$(gcloud config get-value project --format 'value(.)')
+    if [ -z "$gcloud_config_project" ]; then
+        echo "ERROR: \$GOOGLE_CLOUD_PROJECT environment variable is not set!"
+        exit 1
+    else
+        GOOGLE_CLOUD_PROJECT="$gcloud_config_project"
+    fi
+else
+    GOOGLE_CLOUD_PROJECT="$gcloud_config_project"
+fi
+
+# Create a new IAM service account
 gcloud iam service-accounts create gcloudrigkey \
+    --project="$GOOGLE_CLOUD_PROJECT" \
     --description="gcloudrigkey for cloud run" \
     --display-name="gcloudrigkey"
 
-gcloud projects add-iam-policy-binding massive-seer-267723 \
-    --member="serviceAccount:gcloudrigkey@${project_id}.iam.gserviceaccount.com" \
+# Assign it the "roles/editor" role
+gcloud projects add-iam-policy-binding "$GOOGLE_CLOUD_PROJECT" \
+    --project="$GOOGLE_CLOUD_PROJECT" \
+    --member="serviceAccount:gcloudrigkey@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com" \
     --role="roles/editor"
 
+# Get a key for it and save it as "service_account.json"
 gcloud iam service-accounts keys create service_account.json \
-  --iam-account "gcloudrigkey@${project_id}.iam.gserviceaccount.com"
+    --project="$GOOGLE_CLOUD_PROJECT" \
+    --iam-account "gcloudrigkey@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com"
