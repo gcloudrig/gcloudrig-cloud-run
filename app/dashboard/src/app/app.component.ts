5062f61dd03f5d3a33216e5d6caa5a6124517f9f
Owen Schwartz <oschwartz10612@gmail.com>
2021-02-24 11:40:00 -0500

    Add logout

--- app/dashboard/src/app/app.component.ts
+++ app/dashboard/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dashboard';
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
+
 }
